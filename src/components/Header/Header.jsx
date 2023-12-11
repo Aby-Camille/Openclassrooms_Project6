ab8dd8efa5a9a01a156728304f3274e9b9f6d3f6
Camille L <camille0756@gmail.com>
2023-12-11 18:38:16 +0100

    Header component + sass header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Header.scss';
+import logo from '../../images/LOGO.svg'
+
+function Header (){
+    return <div className='header'>header</div>;
+}
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
+import Header from './components/Header/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
       </Routes>
