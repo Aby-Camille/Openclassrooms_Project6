81e56d34a3a9cbd400cc15e2c4339d5617b319d1
Camille L <camille0756@gmail.com>
2023-12-11 19:20:32 +0100

    favicon + document title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { useEffect } from 'react';
+
 function Home (){
+
+    useEffect (() => {
+        document.title = 'Kasa'
+    }, [])
+    
     return <div>Home</div>;
 }
 export default Home;
