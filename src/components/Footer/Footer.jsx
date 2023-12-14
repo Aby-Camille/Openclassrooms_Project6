9f7803e98fdcffea7a37d00884c6d0c645659710
Camille L <camille0756@gmail.com>
2023-12-14 17:07:23 +0100

    Footer jsx update + sass

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Footer.scss';
+import footerLogo from '../../images/logo-footer.svg';
+
+function Footer (){
+    return <footer className='footer'>
+        <img src={footerLogo} alt="logo" className='footer-logo'/>
+        <p className='footer-note'>© 2020 Kasa. All rights reserved</p>
+    </footer>
+}
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +13,7 @@ root.render(
       <Routes>
         <Route path="/" element={<Home/>} />
       </Routes>
+      <Footer/>
     </Router>
   </React.StrictMode>
 );
