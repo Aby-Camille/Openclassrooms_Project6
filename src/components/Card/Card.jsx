b928503f5326663ed10f2eadc999c105bfe9757c
Camille L <camille0756@gmail.com>
2024-01-05 03:57:49 +0100

    card router to housing with json file + card sass

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './Card.scss';
+import { Link } from "react-router-dom";
 
-function Card ({title, location}) {
-    return <div className="card">
-        {title}
-        {location}
-    </div>
+function Card ({title, location, cover}) {
+
+    return <Link to={`/housing/${location}`} className="card">
+        <h3 className="card_title">{title}</h3>
+        <img className="card_img" src= {cover} alt={title}/>   
+    </Link>
 }
 export default Card;
